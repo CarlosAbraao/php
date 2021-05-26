dbe4bcd36ac4349dce9bf26f26fa4f3b5c742c5a
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-05-26 09:35:52 -0300

    Iniciado projeto vale e primeira aula php

---
+++ Aula-Praticando-Php/Aula_01_ImprimindoMensagemNaTela.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Imprimindo Mensagem na tela";
+
+?>
---
+++ projeto-ponto-vale/conexao1.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "projetovale";
+
+$conecta = mysqli_connect($servidor,$usuario,$senha,$banco);
+
+
+
+
+
+?>
