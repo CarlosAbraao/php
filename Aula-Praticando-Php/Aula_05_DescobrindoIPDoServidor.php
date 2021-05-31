bf512035abf5b27da57c17d7dc34095fae139d69
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-05-31 19:22:43 -0300

    Criado IP e operadores lógicos

--- Aula-Praticando-Php/Aula_05_DescobrindoIPDoServidor.php
+++ Aula-Praticando-Php/Aula_05_DescobrindoIPDoServidor.php
@@ -5,7 +5,7 @@
  *  Se quisermos identificar o IP do servidor do usuário que está acessando nosso sistema
  * */
 
- $_SERVER["REMOTE_ADDR"];
+ $_SERVER['REMOTE_ADDR'];
 
 
 
---
+++ Aula-Praticando-Php/Aula_06_Operadores.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *   OPEADORES ARITMÉTICOS
+ *   Adição: (+)
+ *   Subtração: (-)
+ *   Multiplicação: (*)
+ *   Divisão: (/)
+ *   Módulo - resto da divisão: (%)
+ * */
+
+// OPERADORES DE INCLEMENTO E DECLEMENTO
+
+//-operando: troca o sinal do operando.
+//++operando: conhecido como pré-incremento, primeiro incrementa depois realiza a operação.
+//--operando: conhecido como pré-decremento, primeiro decrementa depois realiza a operação.
+//operando++: conhecido como pós-incremento, primeiro faz a operação depois incrementa.
+//operando--: conhecido como pós-decremento, primeiro faz a operação depois decrementa.
+
+
+//  OPERADORES DE ATRIBUIÇÃO
+
+//Atribuição simples(=)
+//Atribuição com adição (+=)
+//Atribuição com subtração (-=)
+//Atribuição com multiplicação (*=)
+//Atribuição com divisão (/=)
+//Atribuição com módulos (%=)
+//Atribuição com concatenação (.=)
+
+
+/** 
+ * OPERADORES LÓGICOS
+ * 
+*and(e) – O resultado verdadeiro só será obtido quando ambos dos dados comparados forem verdadeiros.
+*or(ou) – O resultado verdadeiro só será obtido quando pelo menos um dos dados comparados for verdadeiro.
+*xor – O resultado verdadeiro só será obtido quando pelo menos um dos dados comparados for verdadeiro, mas não ambos.
+*!(não) – O resultado verdadeiro só será obtido se o valor dado não for verdadeiro.
+*&&(e) – Igual ao operador AND.
+*||(ou) – Igual ao operador OR.
+
+**/
+
+
+?>
+
