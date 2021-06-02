42f144867ad196ef6f53cf83a4b6181091564d0a
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-02 16:13:44 -0300

    Laço de repetição for com exemplo de tabuada

---
+++ Aula-Praticando-Php/Aula_13_LacoForComExemplo.php
@@ -0,0 +1,33 @@
+<?php
+
+// ESTRUTURA DE REPETIÇÃO LAÇO FOR
+// EX: DOIS EXEMPLOS SENDO UM UMA TABUADA
+
+
+
+
+echo 'CRIANDO LAÇO CONTADOR ATÉ 10.';
+
+for($i = 0; $i < 10; $i++){
+
+    echo  $i . "<br>";
+    
+
+}
+
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
+
+echo "<b>EXEMPLO DE TABUADA DO NUMERAL 2 USANDO LAÇO DE REPETIÇÃO</b> <br><br><br>";
+for($i = 0; $i < 10; $i++){
+
+    echo "2 x " . $i . " = " . 2*$i . "  <br>" ;
+    
+
+}
+
+
+
+
+
+
+?>
