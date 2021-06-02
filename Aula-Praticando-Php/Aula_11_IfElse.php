a25c9c1fc64fde11caf906dad5812c08e5eab1ba
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-02 14:11:09 -0300

    Criando IfElse

---
+++ Aula-Praticando-Php/Aula_11_IfElse.php
@@ -0,0 +1,21 @@
+<?php
+
+// TESTE DE CONDIÇOES
+//EX: SE A NOTA DO ALUNO FOR MENOR QUE 7 REPROVADO CASO CONTRARIO APROVADOR
+
+$notaAluno = 7.5;
+$valorMedia = 7;
+
+
+
+if ($notaAluno > $valorMedia){
+
+    echo "Aluno Aprovado";
+
+}else
+{
+   echo "Aluno Reprovado";
+}
+
+
+?>
