2d8049ee0a6c3c7a45ab5559a30a1c164d0abd55
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-02 15:50:24 -0300

    Criado Switch Case

---
+++ Aula-Praticando-Php/Aula_12_SwitchCase.php
@@ -0,0 +1,28 @@
+<?php
+
+// OPERAÇÕES - CASO
+// EX: UM JOGO DE FUTEBOL AONDE TENHO VARIOS BOTOES PARA APERTAR
+
+$botoes = 'a';
+
+switch($botoes){
+
+    case 'a':
+        echo "Soco forte";
+        break;
+
+    case 'b':
+        echo "Soco fraco";
+        break;
+
+
+    default:
+
+    echo ("Botão não existe");
+
+}
+
+
+
+
+?>
