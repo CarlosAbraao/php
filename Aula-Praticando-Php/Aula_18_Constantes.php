14d416de50cba7f50b1f441cb553034541cec747
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-08 11:21:18 -0300

    Criando Constantes

---
+++ Aula-Praticando-Php/Aula_18_Constantes.php
@@ -0,0 +1,26 @@
+<?php
+/** */
+
+// CRIANDO UMA CONSTANTE
+
+// EXEMPLO CONSTATE PI
+define("PI", (3.14));
+
+echo PI;
+
+// EXEMPLO DE CONSTANTE GUARDANDO ARRAY
+
+echo "<br>";
+define("BANCO_DADOS", ["sevidor", "usuario", "senha"]);
+
+print_r(BANCO_DADOS);
+
+echo "<br>";
+// CONSTANTE QUE DIZ A VERSÃO DO PHP
+echo "Versão do php     ";
+echo PHP_VERSION;
+
+
+
+
+?>
