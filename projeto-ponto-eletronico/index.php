3acf460f7df2c1f2eb0e44b5012fbfe9f2e5bc87
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-07-08 14:40:53 -0300

    Inicio programa de senha e atendimento em php

--- projeto-ponto-eletronico/index.php
+++ projeto-ponto-eletronico/index.php
@@ -1,10 +1,51 @@
-<!doctype html>
+
 <html>
 <head>
 <meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	
 <title>Sistema de Ponto</title>
 </head>
 
 <body>
+
+	<?php
+	include "topo.php";
+	?>
+	
+<div class="login"><link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div>
+
+  </div>
+</div></div>
+	
+	<?php
+	include "rodape.php";
+	?>
+	
 </body>
 </html>
---
+++ projeto-ponto-eletronico/rodape.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+	<div class="rodape">
+	
+	<div align="center" class="rodapeTexto">Sistema desenvolvido por Carlos Chaves <br>
+		Todos os direitos reservados</div>
+	
+	</div>
+</body>
+</html>
